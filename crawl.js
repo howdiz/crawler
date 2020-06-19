7543e9b87440c528feedf2a02f0c289436155931
hross <hross@linode.com>
2020-06-19 10:36:28 -0400

    outputting urls

--- crawl.js
+++ crawl.js
@@ -3,13 +3,25 @@ const cheerio = require('cheerio');
 var createTextVersion = require("textversionjs");
 
 const url = "https://www.moovweb.com/";
+const keyword = /.{10}XDN/g;
 
 got(url)
   .then((response) => {
-    const pageText = createTextVersion(response.body);
-    const keywords = pageText.match(/XDN/g);
-    console.log(keywords);
+    const body =  response.body;
+    outputKeywordContext(body, keyword);
+    $ = cheerio.load(body);
+    links = $('a');
+    $(links).each(function(i, link){
+        console.log($(link).attr('href'));
+      });
   })
   .catch((error) => {
     console.log(error);
   });
+
+  function outputKeywordContext(content, keyword ) {
+    const pageText = createTextVersion(content);
+    const keywords = pageText.match(keyword);
+    console.log(keywords);
+
+  }
