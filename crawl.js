bd8915134402fcf35fb5e599628c8263a19dd6fe
hross <hross@linode.com>
2020-06-19 11:48:13 -0400

    semi works

--- crawl.js
+++ crawl.js
@@ -1,27 +1,45 @@
 const got = require('got');
 const cheerio = require('cheerio');
-var createTextVersion = require("textversionjs");
+const createTextVersion = require("textversionjs");
 
 const url = "https://www.moovweb.com/";
 const keyword = /.{10}XDN/g;
 
-got(url)
-  .then((response) => {
-    const body =  response.body;
-    outputKeywordContext(body, keyword);
-    $ = cheerio.load(body);
-    links = $('a');
-    $(links).each(function(i, link){
-        console.log($(link).attr('href'));
-      });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+var linksCrawled = [];
+
+
+function getUrl (url) {
+    if (!linksCrawled.includes(url)) {
+        console.log(url);
+        linksCrawled.push(url);
+        got(url)
+        .then((response) => {
+            const body =  response.body;
+            outputKeywordContext(body, keyword);
+            getLinks(body, url);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    }
+}
 
   function outputKeywordContext(content, keyword ) {
     const pageText = createTextVersion(content);
     const keywords = pageText.match(keyword);
     console.log(keywords);
-
   }
+
+  function getLinks (content, url) {
+    $ = cheerio.load(content);
+    allLinks = $('a');
+    $(allLinks).each(function(i, link){
+        let href = $(link).attr('href');
+        //TODO Handle relative links
+        if( href && href.startsWith(url) ) {
+            getUrl(href);
+        }
+      });
+}
+
+getUrl(url);
